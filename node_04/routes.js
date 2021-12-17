ea5e580f62849428a044794ad6d8c349c8f9ead1
AidaShuki <91296308+AidaShuki@users.noreply.github.com>
2021-12-17 09:16:59 +0900

    Add files via upload

---
+++ node_04/routes.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const router = express.Router()
+
+const default_login_name = process.env.LOGIN_NAME
+const default_passsword = process.env.PASSWORD
+
+router.get('/login', (req, res) => {
+    res.render('login/index.ejs')
+})
+
+router.post('/auth', (req, res) => {
+    let message = 'ログインできません'
+    const login_name = req.body.login_name
+    const password = req.body.password
+
+    if (login_name == default_login_name
+        && password == default_passsword) {
+        message = 'ログインしました'
+    }
+    res.send(message)
+})
+
+router.get('/', (req, res) => {
+    let data = {}
+    data.title = 'トップページ'
+    res.render('index.ejs', data)
+})
+
+router.get('/profile', (req, res) => {
+    res.render('profile/index.ejs')
+})
+
+router.get('/item/:id', (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    res.render('item/show.ejs')
+})
+
+module.exports = router
